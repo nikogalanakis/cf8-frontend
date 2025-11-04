c53658cce75f48967a47b0f7b6d4ac2c4886c71b
NGALANAKIS <galanakis.delfi@aueb.gr>
2025-11-04 21:03:30 +0200

    initialize

---
+++ src/app/app.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Step2PersonTable } from './components/step2-person-table/step2-person-table';  
+
+
+@Component({
+  selector: 'app-root',
+  imports: [Step2PersonTable],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+
+  
+}
+
+
